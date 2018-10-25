0d7fdb57b54451591c152a09325d5290be79277f
Goblin01 <cass.taylor1@gmail.com>
2018-10-25 10:29:00 -0500

    Create Delete a List's Head Node.java

---
+++ Delete a List's Head Node.java
@@ -0,0 +1,6 @@
+// java.util.* and java.util.streams.* have been imported for this problem.
+// You don't need any other imports.
+
+public ListNode deleteAtHead(ListNode head) {
+    return (head == null) ? null : head.next;
+}
